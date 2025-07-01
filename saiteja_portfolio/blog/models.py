9446c87cfbe807011ddd056b68d159329ee3f594
Mantha Sai Teja <151930706+SaiTejaMantha03@users.noreply.github.com>
2025-07-01 19:48:28 +0530

    Update models.py

--- saiteja_portfolio/blog/models.py
+++ saiteja_portfolio/blog/models.py
@@ -13,3 +13,18 @@ class BlogPost(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self): return self.title
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    project_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
